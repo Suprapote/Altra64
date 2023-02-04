53de034570abbaf1fe71754018d909e9eb51da29
Suprapote <111246491+Suprapote@users.noreply.github.com>
2023-02-04 11:32:52 +0100

    Splash screen deleted

--- src/main.c
+++ src/main.c
@@ -1178,7 +1178,7 @@ void loadgbrom(display_context_t disp, TCHAR *rom_path)
             f_close(&romfile);
 
             boot_cic = CIC_6102;
-            boot_save = 2; //flash
+            boot_save = 5; //flash
             force_tv = 0;  //no force
             cheats_on = 0; //cheats off
             checksum_fix_on = 0;
